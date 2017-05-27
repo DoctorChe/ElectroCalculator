e9c56abf0c9f9c56bf154b8edaf0bf4c1ed450be
DoctorChe <duncan@fromru.com>
2017-05-28 01:35:46 +0300

    Add files via upload

---
+++ ElectroCalc.py
@@ -0,0 +1,29 @@
+import sys
+# Импортируем наш интерфейс из файла
+from MainWindow import *
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class MyWin(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        # Здесь прописываем событие нажатия на кнопку
+        self.ui.pushButton.clicked.connect(self.my_function)
+
+        self.ui.lineEdit.setValidator(QtGui.QDoubleValidator(0.99, 99.99, 2))
+
+    # Пока пустая функция которая выполняется
+    # при нажатии на кнопку
+    def my_function(self):
+        # self.ui.lineEdit_3.setText(str(float(self.ui.lineEdit.text()) + float(self.ui.lineEdit_2.text())))
+        a = float(self.ui.lineEdit.text())
+        # self.ui.lineEdit_3.setText(self.ui.lineEdit.text() * 5)
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    myapp = MyWin()
+    myapp.show()
+    sys.exit(app.exec_())
