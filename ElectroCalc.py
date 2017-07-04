172a7eaa4bd43caa1673efdcbafdbc4e5590fefc
Андрей Лихобабин <a.likhobabin@corp.npospec.ru>
2017-07-04 17:31:28 +0300

    Edd menu

--- ElectroCalc.py
+++ ElectroCalc.py
@@ -2,6 +2,7 @@ import sys
 # Импортируем наш интерфейс из файла
 from ui_mainwindow import Ui_MainWindow
 from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QAction
 #from PyQt5 import QtCore, QtGui
 #from PyQt5 import uic
 # from PyQt5.uic import loadUi
@@ -18,8 +19,15 @@ class MyWin(QtWidgets.QMainWindow):
 #        widget = loadUi('demo.ui')
 #        self.ui = uic.loadUi('Ui_MainWindow.ui')
 
+        # Событие - запуск вычисления
+        self.ui.action_Calculate.triggered.connect(self.my_function)
+
+        # Событие - выход из программы
+        self.ui.action_Exit.triggered.connect(self.close)
+
         # Здесь прописываем событие нажатия на кнопку
-        self.ui.pushButton.clicked.connect(self.my_function)
+        self.ui.pushButton.clicked.connect(self.ui.action_Exit)
+#        self.ui.pushButton.addAction(self.ui.action_Exit)
 
 #        self.ui.lineEdit.setValidator(QtGui.QDoubleValidator(0.99, 99.99, 2))
 
@@ -31,9 +39,14 @@ class MyWin(QtWidgets.QMainWindow):
             b = float(self.ui.lineEdit_B.text())
         except ValueError:
             self.statusBar().showMessage('Введите число.')
+        except Exception:
+            # Заглушка для всех ошибок
+            print('Это что ещё такое?')
         else:
             c = short_current_calc(a, b)
             self.ui.lineEdit_C.setText(str(c))
+        finally:
+            pass
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
