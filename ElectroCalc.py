68a1b2270fa8c7c4765fdeb824aceb30164b5f84
Duncan <duncan@fromru.com>
2017-09-11 01:59:15 +0300

    Edd settings support

--- ElectroCalc.py
+++ ElectroCalc.py
@@ -1,7 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"Программа для вычисления токов короткого замыкания"
+
 import sys
 # Импортируем наш интерфейс из файла
 from ui_mainwindow import Ui_MainWindow
-from PyQt5 import QtWidgets
+#from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QApplication#, QWidget
+from PyQt5.QtCore import QSettings
 # from PyQt5.QtWidgets import QAction
 # from PyQt5 import QtCore, QtGui
 # from PyQt5 import uic
@@ -10,11 +16,21 @@ from PyQt5 import QtWidgets
 import short_circuit_current_calculation as sccc
 
 
-class MyWin(QtWidgets.QMainWindow):
-    def __init__(self, parent=None):
-        QtWidgets.QWidget.__init__(self, parent)
+#class MyWin(QtWidgets.QMainWindow):
+class MyWin(QMainWindow):
+    """
+    Основной класс программы
+    """
+    def __init__(self, iniFile, parent=None):
+#        QtWidgets.QWidget.__init__(self, parent)
+#        QWidget.__init__(self, parent)
+        QMainWindow.__init__(self, parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.settings = QSettings(iniFile, QSettings.IniFormat)
+        self.settings.setIniCodec("utf-8")
+        # Чтение настроек
+        self.read_settings()
 
 #        uic.loadUi("MainWindow.ui", self)
 #        widget = loadUi('demo.ui')
@@ -33,42 +49,101 @@ class MyWin(QtWidgets.QMainWindow):
 
 #        self.ui.lineEdit.setValidator(QtGui.QDoubleValidator(0.99, 99.99, 2))
 
-    # Пока пустая функция которая выполняется
-    # при нажатии на кнопку
+
+    def closeEvent(self, e): # pylint: disable=invalid-name
+        """
+        Обработка события закрытия программы
+        """
+        # Write data to config file
+        self.save_settings()
+        e.accept()
+
+    def read_settings(self):
+        """
+        Чтение настроек
+        """
+#        self.inputs = {}
+#        self.params = []
+#        ini.beginGroup("Common")
+#        wt = ini.value('Title','')
+#        if wt != '': self.setWindowTitle(wt)
+#        ini.endGroup()
+        self.settings.beginGroup("Transformer")
+        self.lineEdit_U_sr_VN_State = self.settings.value("U_sr_VN", 10)
+        self.ui.lineEdit_U_sr_VN.setText(str(self.lineEdit_U_sr_VN_State))
+        self.lineEdit_U_sr_NN_State = self.settings.value("U_sr_NN", 400)
+        self.ui.lineEdit_U_sr_NN.setText(str(self.lineEdit_U_sr_NN_State))
+        self.settings.endGroup()
+        self.settings.beginGroup("3ph_short_circuit_current_HV")
+        self.lineEdit_Sk_IkVN_Xs_State = self.settings.value("Sk_IkVN_Xs_Iotklnom", 0)
+        self.ui.lineEdit_Sk_IkVN_Xs.setText(str(self.lineEdit_Sk_IkVN_Xs_State))
+        self.comboBox_Sk_IkVN_Xs_State = self.settings.value("xc_mode", 2)
+        self.ui.comboBox_Sk_IkVN_Xs.setCurrentIndex(int(self.comboBox_Sk_IkVN_Xs_State))
+        self.settings.endGroup()
+#        chBoxState = settings.value('chBoxState', QtCore.Qt.Checked)
+#        self.chB.setCheckState(int(chBoxState))
+ 
+
+    def save_settings(self):
+        """
+        Сохранение настроек
+        """
+        self.settings.beginGroup("Transformer")
+        self.settings.setValue('U_sr_VN', self.ui.lineEdit_U_sr_VN.text())
+        self.settings.setValue('U_sr_NN', self.ui.lineEdit_U_sr_NN.text())
+        self.settings.endGroup()
+        self.settings.beginGroup("3ph_short_circuit_current_HV")
+        self.settings.setValue('Sk_IkVN_Xs_Iotklnom', self.ui.lineEdit_Sk_IkVN_Xs.text())
+        self.settings.setValue('xc_mode', self.ui.comboBox_Sk_IkVN_Xs.currentIndex())
+        self.settings.endGroup()
+#        self.layoutSettings.sync()
+    
+
+#    def loadIni(self, iniFile):
+#        ini = QSettings(iniFile, QSettings.IniFormat)
+#        ini.setIniCodec("utf-8")
+
     def my_function(self):
+        """
+        Основная функция программы
+        """
         self.ui.lineEdit_Ip0_3ph.clear()
         try:
-            U_sr_VN = float(self.ui.lineEdit_U_sr_VN.text())
-            U_sr_NN = float(self.ui.lineEdit_U_sr_NN.text())
+            U_sr_VN = float(self.ui.lineEdit_U_sr_VN.text()) # pylint: disable=invalid-name
+            U_sr_NN = float(self.ui.lineEdit_U_sr_NN.text()) # pylint: disable=invalid-name
 #            R_1sum = float(self.ui.lineEdit_R_1sum.text())
 #            X_1sum = float(self.ui.lineEdit_X_1sum.text())
 
-            Sk_IkVN_Xs_Iotklnom = float(self.ui.lineEdit_Sk_IkVN_Xs.text())
+            Sk_IkVN_Xs_Iotklnom = float(self.ui.lineEdit_Sk_IkVN_Xs.text()) # pylint: disable=invalid-name
         except ValueError:
             self.statusBar().showMessage('Введите исходные данные.')
 #        except Exception:
             # Заглушка для всех ошибок
 #            print('Это что ещё такое?')
         else:
-            def switch_Xc(x):
+            def switch_Xc(xc_mode): # pylint: disable=invalid-name
+                """
+                Определение положения переключателя для определения
+                тока КЗ на выводах ВН
+                """
                 return{
-                        0: "Sk",
-                        1: "IkVN",
-                        2: "Xs"
-                        }.get(x, "Iotklnom")
+                    0: "Sk",
+                    1: "IkVN",
+                    2: "Xs"
+                    }.get(xc_mode, "Iotklnom")
             switch = switch_Xc(self.ui.comboBox_Sk_IkVN_Xs.currentIndex())
 #            Ip0_3ph = sccc.calc_Ip0_3ph(R_1sum, X_1sum, U_sr_NN)
-            Ip0_3ph = sccc.calc_short_current(
-                    switch, Sk_IkVN_Xs_Iotklnom, U_sr_NN, U_sr_VN,
-                    Pk_nom=0, U_NN_nom=0.4, St_nom=0, u_k=0,
-                    Pr_nom_delta=0, Ir_nom=0, f=0, L=0, M=0,
-                    RtA=0, XtA=0,
-                    Rkv=0, Xkv=0,
-                    Rsh=0, Xsh=0,
-                    Rk=0,
-                    R_1kb=0, X_1kb=0,
-                    Rvl=0, Xvl=0,
-                    Rd=0)
+            Ip0_3ph = sccc.calc_short_current( # pylint: disable=invalid-name
+                switch, Sk_IkVN_Xs_Iotklnom, U_sr_NN, U_sr_VN,
+                Pk_nom=0, U_NN_nom=0.4, St_nom=0, u_k=0,
+                Pr_nom_delta=0, Ir_nom=0, f=0, L=0, M=0,
+                RtA=0, XtA=0,
+                Rkv=0, Xkv=0,
+                Rsh=0, Xsh=0,
+                Rk=0,
+                R_1kb=0, X_1kb=0,
+                Rvl=0, Xvl=0,
+                Rd=0)
             self.ui.lineEdit_Ip0_3ph.setText(str(round(Ip0_3ph, 1)))
         finally:
             # Выбирается вкладка "Результаты"
@@ -81,7 +156,8 @@ class MyWin(QtWidgets.QMainWindow):
 #        self.ui.lineEdit_Ip0_3ph.clear()
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    myapp = MyWin()
+    QApplication.setDesktopSettingsAware(False)
+    app = QApplication(sys.argv) # pylint: disable=invalid-name
+    myapp = MyWin("last_values.ini") # pylint: disable=invalid-name
     myapp.show()
     sys.exit(app.exec_())
